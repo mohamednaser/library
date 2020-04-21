90b20adc0288e99b890f388e96d96f5eb41cfd29
Salvador <salvador.on@outlook.com>
2020-04-21 17:00:37 -0300

    add linter

--- JS/firebase.js
+++ JS/firebase.js
@@ -55,7 +55,7 @@ class customFirebase {
 
         cardDetail += " </button>";
         cardDetail +=
-          "<button onclick='removeBook(" + key + ")'class='btn btn-danger'>";
+          "<button onclick='removeBook(" + key + ")'class='btn btn-danger ml-2'>";
         cardDetail += "Remove";
 
         cardDetail += " </button>";
